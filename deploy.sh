2089b9e4c2739452879f55b793e7efd647ee9d66
Matt Banks <mjbanks@gmail.com>
2014-08-11 12:09:47 -0400

    change plugin slug since wordpress.org repo is different than plugin name on github

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
 # main config
-PLUGINSLUG=${PWD##*/} # returns basename of current directory
+PLUGINSLUG="woocommerce-local-pickup-time-select" # returns basename of current directory
 CURRENTDIR=`pwd`
 MAINFILE="woocommerce-local-pickup-time.php" # this should be the name of your main php file in the wordpress plugin
 
