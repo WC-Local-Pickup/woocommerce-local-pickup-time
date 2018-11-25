364195530be2b8ec7338b74de565c36e35efe537
tim.nolte <tim.nolte@ndigitals.com>
2018-11-25 16:01:57 -0500

    * Updates copyright year and version numbers. * Fixes code formatting to align to coding standards.

--- uninstall.php
+++ uninstall.php
@@ -6,10 +6,10 @@
  * @author    Matt Banks <mjbanks@gmail.com>
  * @license   GPL-2.0+
  * @link      http://mattbanks.me
- * @copyright 2014-2017 Matt Banks
+ * @copyright 2014-2018 Matt Banks
  */
 
-// If uninstall not called from WordPress, then exit
+// If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
--- woocommerce-local-pickup-time.php
+++ woocommerce-local-pickup-time.php
@@ -10,22 +10,21 @@
  * @author    Matt Banks <mjbanks@gmail.com>
  * @license   GPL-2.0+
  * @link      http://mattbanks.me
- * @copyright 2014-2017 Matt Banks
+ * @copyright 2014-2018 Matt Banks
  *
  * @wordpress-plugin
- * Plugin Name: 	  WooCommerce Local Pickup Time Select
- * Plugin URI:  	  http://mattbanks.me
- * Description: 	  Add an an option to WooCommerce checkout pages for Local Pickup that allows the user to choose a pickup time.
- * Version:     	  1.3.1
- * Author:      	  Matt Banks
- * Author URI:  	  http://mattbanks.me
- * Text Domain: 	  woocommerce-local-pickup-time
- * License:     	  GPL-2.0+
+ * Plugin Name:       WooCommerce Local Pickup Time Select
+ * Plugin URI:        http://mattbanks.me
+ * Description:       Add an an option to WooCommerce checkout pages for Local Pickup that allows the user to choose a pickup time.
+ * Version:           1.3.2
+ * Author:            Matt Banks
+ * Author URI:        http://mattbanks.me
+ * Text Domain:       woocommerce-local-pickup-time
+ * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: 	  /languages
+ * Domain Path:       /languages
  * GitHub Plugin URI: https://github.com/mattbanks/woocommerce-local-pickup-time
  */
-
 
 /**
  * If this file is called directly, abort.
@@ -37,35 +36,39 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * Check if WooCommerce is active
- **/
+ */
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-	/*----------------------------------------------------------------------------*
+	/**
+	 * ----------------------------------------------------------------------------
 	 * Public-Facing Functionality
-	 *----------------------------------------------------------------------------*/
+	 * ----------------------------------------------------------------------------
+	 */
 
-	/*
+	/**
 	 * Require public facing functionality
 	 */
 	require_once( plugin_dir_path( __FILE__ ) . 'public/class-local-pickup-time.php' );
 
-	/*
+	/**
 	 * Register hooks that are fired when the plugin is activated or deactivated.
 	 * When the plugin is deleted, the uninstall.php file is loaded.
 	 */
 	register_activation_hook( __FILE__, array( 'Local_Pickup_Time', 'activate' ) );
 	register_deactivation_hook( __FILE__, array( 'Local_Pickup_Time', 'deactivate' ) );
 
-	/*
+	/**
 	 * Get instance
 	 */
 	add_action( 'plugins_loaded', array( 'Local_Pickup_Time', 'get_instance' ) );
 
-	/*----------------------------------------------------------------------------*
+	/**
+	 * ----------------------------------------------------------------------------
 	 * Dashboard and Administrative Functionality
-	 *----------------------------------------------------------------------------*/
+	 * ----------------------------------------------------------------------------
+	 */
 
-	/*
+	/**
 	 * Require admin functionality
 	 */
 	if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
@@ -74,5 +77,4 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		add_action( 'plugins_loaded', array( 'Local_Pickup_Time_Admin', 'get_instance' ) );
 
 	}
-
 }
