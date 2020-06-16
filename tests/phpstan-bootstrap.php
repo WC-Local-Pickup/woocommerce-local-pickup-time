bd6a39dd3cda6e335d0459e7ab2535359ccf1b59
Tim Nolte <tim.nolte@ndigitals.com>
2020-06-15 23:15:58 -0400

    * Fixes code formatting of PHPStan bootstrap file.

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * phpstan bootstrap file.
+ * Phpstan bootstrap file.
+ *
+ * @package   Local_Pickup_Time
+ * @author    Matt Banks <mjbanks@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://mattbanks.me
+ * @copyright 2014-2020 Matt Banks
  */
 
+// Define WooCommerce path for autoloading in phpstan.
 defined( 'WC_ABSPATH' ) || define( 'WC_ABSPATH', 'vendor/woocommerce/woocommerce/' );
